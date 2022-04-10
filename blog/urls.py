50bb01164f416a808d7de8b12205e0d57c6b4954
oppenheimermd <michieloppenheimer@gmail.com>
2022-04-10 14:01:41 +0100

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from importlib.resources import path
+from django.urls import path
+from . import views
+
+# application namespace.  Allows us to organize URL's by application and use the
+# 'app_name' when referring to it.
+app_name = 'blog'
+
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'),
+]
