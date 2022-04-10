630fba12a237065473f312915e008f1c3df90c22
oppenheimermd <michieloppenheimer@gmail.com>
2022-04-10 17:28:04 +0100

    initial layout and views added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
+import imp
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -22,6 +24,14 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manage.
     published = PublishedManager() #Our custom manage.
+
+    # Canonical URL for Post objects.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                               self.publish.month,
+                               self.publish.day,
+                               self.slug])
 
     class Meta:
         ordering = ('-publish',)
