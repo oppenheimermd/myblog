153b54c03ce33384efe725e0f6f081dbbbb3c50f
oppenheimermd <michieloppenheimer@gmail.com>
2022-04-13 17:10:15 +0100

    blog/post/detail added comment forms, updated template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from typing import List
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 def post_detail(request, year, month, day, post):
@@ -11,10 +12,50 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
+    
+    # List of active comments for this post(a QuerySet)
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+        
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                  'comments':comments,
+                  'new_comment': new_comment,
+                  'comment_form': comment_form})
 
+def post_share(request, post_id):
+    #  Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.method == 'POST':
+        #  Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #  Form fields passed validation
+            cd = form.cleaned_data
+            #   Send email
+    else:
+        form = EmailPostForm()
+    
+    return render(request, 'blog/post/share.htm',
+                    {'post':post,
+                    'form':form})
+                    
 #   Class based view             
 class PostListView(ListView):
     queryset = Post.published.all()
